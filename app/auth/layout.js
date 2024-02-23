1e1549821ddba059c57154f1e3cad90458371f40
Luca <luca.debionne@yahoo.fr>
2024-02-23 11:23:27 +0100

    maj

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -4,7 +4,7 @@ export default async function AuthLayout({ children, login, register }) {
       <div className="lg:w-1/2 lg:h-screen flex justify-center items-center">
         {children}
       </div>
-      <div className="lg:w-1/2 lg:h-screen flex justify-center lg:bg-default-50 items-center">
+      <div className="lg:w-1/2 lg:h-screen flex justify-center lg:bg-default-50 items-center px-6">
         {login}
       </div>
     </div>
