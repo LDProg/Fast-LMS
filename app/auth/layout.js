d7ccc48c600053c4ea8d632bc427e51a1b648910
Luca <luca.debionne@yahoo.fr>
2024-02-23 11:24:59 +0100

    maj

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -1,6 +1,6 @@
 export default async function AuthLayout({ children, login, register }) {
   return (
-    <div className="h-screen flex flex-col lg:flex-row items-center justify-center bg-background">
+    <div className="h-screen flex flex-col lg:flex-row items-center justify-center bg-background overflow-hidden">
       <div className="lg:w-1/2 lg:h-screen flex justify-center items-center">
         {children}
       </div>
