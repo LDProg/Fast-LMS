6d041e19eaa0eccf50a6852b17c680b727492d72
Luca <luca.debionne@yahoo.fr>
2024-02-26 08:53:49 +0100

    maj

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,5 +1,12 @@
 import { SparklesTitle } from "../_components/sparkles-title";
 
 export default function AuthPage() {
+  return (
+    <div className="h-fit w-full bg-background flex flex-col items-center justify-center overflow-hidden rounded-md px-10">
+      <h1 className="md:text-6xl text-5xl lg:text-7xl font-50 text-center text-white lg:m-0 mb-[20%] whitespace-nowrap">
+        FAST LMS
+      </h1>
+    </div>
+  );
   return <SparklesTitle />;
 }
